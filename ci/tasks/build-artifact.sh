4bd95ca116ec3999abe3949ad475484e1f17f275
Brian Byers <bbyers@pivotal.io>
2016-06-21 14:41:33 -0500

    initial commit

---
+++ ci/tasks/build-artifact.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+export GRADLE_OPTS=-Dorg.gradle.native=false
+version=`cat version/number`
+cd git-repo
+gradle assemble -PVersion=$version
+cp build/libs/*.war ../artifact-dir/
