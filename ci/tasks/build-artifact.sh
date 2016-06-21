c38b4001f8108652ad9d474d6a2f290cf5efdc69
Brian Byers <bbyers@pivotal.io>
2016-06-21 15:13:23 -0500

    updating version

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,4 +5,4 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd git-repo
 gradle assemble -PVersion=$version
-cp build/libs/*.war ../artifact-dir/
+cp build/libs/*.jar ../artifact-dir/
