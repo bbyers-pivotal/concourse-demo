0577d15dae26160780011fbbdb6b0e68b73db267
Brian Byers <bbyers@pivotal.io>
2016-06-23 09:20:02 -0500

    adding task to clean up env on success

---
+++ ci/tasks/delete-test-env.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# get_code="curl -I $CONCOURSE_DEMO_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
+# status_code=`eval $get_code`
+# if [ "$status_code" != "200" ]
+# then
+#   echo "Expect status code from $CONCOURSE_DEMO_URL as 200, but got $status_code"
+#   exit 1
+# fi
+version=`cat version/number | sed -e 's/\./_/g'`
+dash="-"
+
+cf api $CF_API --skip-ssl-validation
+
+echo "Login....."
+cf auth $CF_USER $CF_PASS
+
+echo "Delete Space"
+cf target -o $CF_ORG -s $CF_SPACE$dash$version
+
+cf delete -f $CF_SPACE$dash$version
