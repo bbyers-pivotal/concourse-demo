533f53bfba8c5eb17aac8c512aab203f2002f7a5
Brian Byers <bbyers@pivotal.io>
2016-06-23 17:37:12 -0500

    added user

--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -5,6 +5,10 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd git-repo
 # cp -r git-repo/* modified-git-repo/
+
+git config --global user.email "bbyers@pivotal.io"
+git config --global user.name "Brian Byers (Automated)"
+
 echo ${version} > changeme
 git add changeme
 git commit -m 'triggering change'
