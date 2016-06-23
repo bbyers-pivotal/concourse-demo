f1b1f925342af619435fb5f2c20a1ebe15ddaef8
Brian Byers <bbyers@pivotal.io>
2016-06-23 16:07:58 -0500

    trying something

--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -4,5 +4,7 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 
-cp -r git-repo/* modified-git-repo/
-echo ${version} > modified-git-repo/changeme
+# cp -r git-repo/* modified-git-repo/
+echo ${version} > git-repo/changeme
+git add git-repo/changeme
+git commit -m 'triggering change'
