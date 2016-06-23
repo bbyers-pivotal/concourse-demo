10bd0ce7a5809f936f239ced0c2cd622667da18f
Brian Byers <bbyers@pivotal.io>
2016-06-23 14:27:00 -0500

    trying a restart

--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -4,5 +4,5 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd git-repo
-gradle assemble -Pversion=$version
-cp build/libs/*.jar ../artifact-dir/
+
+cat version > git-repo/changeme
