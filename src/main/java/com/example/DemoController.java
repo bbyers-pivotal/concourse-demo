1eac6df7d4c71db9006e77b3eeac3c3d7d0b0ad3
Brian Byers <bbyers@pivotal.io>
2016-06-22 20:50:21 -0500

    return something from controller

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -3,14 +3,11 @@ package com.example;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by bbyers on 6/22/16.
- */
 @RestController
 public class DemoController {
 
     @RequestMapping("/")
-    public void home() {
-
+    public String home() {
+        return "hi there";
     }
 }
