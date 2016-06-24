bf25c418163200d11173d5f9d51855cb0fe91605
Brian Byers <bbyers@pivotal.io>
2016-06-24 10:24:29 -0500

    trying something

--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -3,15 +3,21 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
-cd git-repo
-# cp -r git-repo/* modified-git-repo/
+
+mkdir ~/.ssh
+cat $SSH_KEY > ~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa
+
+cd ~/
 
 git config --global user.email "bbyers@pivotal.io"
 git config --global user.name "Brian Byers (Automated)"
 
+git clone ${GIT_REPO} repo
+cd repo
+
 echo ${version} > changeme
 git add changeme
 git commit -m 'triggering change'
-
-cp -r * ../another-repo
-ls -lah ../another-repo
+git push
