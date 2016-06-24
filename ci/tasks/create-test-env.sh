0bb89198b84a6f29613f209c9b64a6df1a75d29b
Brian Byers <bbyers@pivotal.io>
2016-06-24 10:33:34 -0500

    cleanup

--- ci/tasks/create-test-env.sh
+++ ci/tasks/create-test-env.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# get_code="curl -I $CONCOURSE_DEMO_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
-# status_code=`eval $get_code`
-# if [ "$status_code" != "200" ]
-# then
-#   echo "Expect status code from $CONCOURSE_DEMO_URL as 200, but got $status_code"
-#   exit 1
-# fi
 version=`cat version/number | sed -e 's/\./_/g'`
 dash="-"
 
--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 
 mkdir ~/.ssh
