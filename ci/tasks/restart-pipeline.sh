4560d662f0eb770d5e704dc93d89744b2bc7b8eb
Brian Byers <bbyers@pivotal.io>
2016-06-24 10:34:35 -0500

    smaller chunk

--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -8,15 +8,15 @@ cat $SSH_KEY > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa
 
-cd ~/
-
-git config --global user.email "bbyers@pivotal.io"
-git config --global user.name "Brian Byers (Automated)"
-
-git clone ${GIT_REPO} repo
-cd repo
-
-echo ${version} > changeme
-git add changeme
-git commit -m 'triggering change'
-git push
+# cd ~/
+#
+# git config --global user.email "bbyers@pivotal.io"
+# git config --global user.name "Brian Byers (Automated)"
+#
+# git clone ${GIT_REPO} repo
+# cd repo
+#
+# echo ${version} > changeme
+# git add changeme
+# git commit -m 'triggering change'
+# git push
