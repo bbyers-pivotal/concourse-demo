e5a20439159a5b46f86b6c9a486e2774127f7674
Brian Byers <bbyers@pivotal.io>
2016-06-24 10:37:18 -0500

    adding back

--- ci/tasks/restart-pipeline.sh
+++ ci/tasks/restart-pipeline.sh
@@ -5,18 +5,18 @@ version=`cat version/number`
 
 mkdir ~/.ssh
 echo $SSH_KEY > ~/.ssh/id_rsa
-# chmod 0600 ~/.ssh/id_rsa
-# cat ~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa
 
-# cd ~/
-#
-# git config --global user.email "bbyers@pivotal.io"
-# git config --global user.name "Brian Byers (Automated)"
-#
-# git clone ${GIT_REPO} repo
-# cd repo
-#
-# echo ${version} > changeme
-# git add changeme
-# git commit -m 'triggering change'
-# git push
+cd ~/
+
+git config --global user.email "bbyers@pivotal.io"
+git config --global user.name "Brian Byers (Automated)"
+
+git clone ${GIT_REPO} repo
+cd repo
+
+echo ${version} > changeme
+git add changeme
+git commit -m 'triggering change'
+git push
